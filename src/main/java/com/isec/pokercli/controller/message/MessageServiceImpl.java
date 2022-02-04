3be7badeb3e9e6ae1dc9642c75cabb149df9e7b8
Pedro Pereira <pbp333@gmail.com>
2022-02-04 19:56:57 +0000

    message creation mechanism

--- src/main/java/com/isec/pokercli/controller/message/MessageServiceImpl.java
+++ src/main/java/com/isec/pokercli/controller/message/MessageServiceImpl.java
@@ -1,10 +1,26 @@
 package com.isec.pokercli.controller.message;
 
+import com.isec.pokercli.model.entity.message.Message;
+import com.isec.pokercli.model.entity.user.User;
+import com.isec.pokercli.model.session.DbSessionManager;
+
 public class MessageServiceImpl implements MessageService {
 
     @Override
     public void deliverMessage(String origin, String destination, String message) {
 
+        var originUser = User.getByUsername(origin);
+        if (originUser == null) {
+            throw new IllegalArgumentException("User does not exist - " + origin);
+        }
+
+        var destinationUser = User.getByUsername(destination);
+        if (destinationUser == null) {
+            throw new IllegalArgumentException("User does not exist - " + destination);
+        }
+        Message.from(originUser.getId(), destinationUser.getId(), message);
+
+        DbSessionManager.getUnitOfWork().commit();
     }
 
     @Override
